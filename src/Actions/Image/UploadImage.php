9a9b4e38f2c2f55e8b85184163ac774900527343
Rodolfo Berrios <inbox@rodolfoberrios.com>
2020-08-05 12:38:39 -0400

    servicing wip

--- src/Actions/Image/UploadImage.php
+++ src/Actions/Image/UploadImage.php
@@ -14,16 +14,36 @@ declare(strict_types=1);
 namespace Chevereto\Actions\Image;
 
 use Chevere\Components\Response\ResponseSuccess;
+use Chevere\Components\Service\ServiceProviders;
 use Chevere\Interfaces\Response\ResponseInterface;
+use Chevere\Interfaces\Service\ServiceableInterface;
+use Chevere\Interfaces\Service\ServiceProvidersInterface;
 use Chevere\Interfaces\Workflow\ActionInterface;
+use Psr\Log\LoggerInterface;
 
-class UploadImage implements ActionInterface
+class UploadImage implements ActionInterface, ServiceableInterface
 {
     // private string $filename;
+
+    private LoggerInterface $logger;
 
     public function __construct(string $filename)
     {
         // $this->filename = $filename;
+    }
+
+    public function getServiceProviders(): ServiceProvidersInterface
+    {
+        return (new ServiceProviders($this))
+            ->withAdded('withLogger');
+    }
+
+    public function withLogger(LoggerInterface $logger): self
+    {
+        $new = clone $this;
+        $new->logger = $logger;
+
+        return $new;
     }
 
     public function execute(): ResponseInterface
